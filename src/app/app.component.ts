b7a6fb71483e2c0b14a8c8c060832221fcb091b3
c5apple <c5apple_party@yahoo.co.jp>
2019-08-25 22:57:05 +0900

    video 再生/停止

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
 
+  @ViewChild('videoPlayer') public videoPlayer: ElementRef;
+
+  isStarted = false;
+
+  ngOnInit(): void {
+    this.videoPlayer.nativeElement.onloadeddata = () => {
+      this.play();
+    }
+  }
+
+  toggleVideo(event: any) {
+    if (this.isStarted) {
+      this.pause();
+    } else {
+      this.play();
+    }
+  }
+
+  play(): void {
+    this.videoPlayer.nativeElement.play();
+    this.isStarted = true;
+  }
+
+  pause(): void {
+    this.videoPlayer.nativeElement.pause();
+  }
+
+  isPaused(): boolean {
+    return this.videoPlayer.nativeElement.paused;
+  }
 }
