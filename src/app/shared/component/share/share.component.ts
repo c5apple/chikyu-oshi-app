1a7e9d4865ffc940d41b81e3d17961b2fcf54c97
c5apple <c5apple_party@yahoo.co.jp>
2019-08-28 01:10:30 +0900

    #1 シェア処理

--- src/app/shared/component/share/share.component.ts
+++ src/app/shared/component/share/share.component.ts
@@ -15,4 +15,44 @@ export class ShareComponent implements OnInit {
   ngOnInit() {
   }
 
+  /**
+   * Facebookでシェア
+   */
+  shareFacebook(): void {
+    const url = 'https://www.facebook.com/sharer.php?u=' + encodeURIComponent(location.href);
+    window.open(url, '_blank', 'noopener');
+  }
+
+  /**
+   * Twitterでシェア
+   */
+  shareTwitter(): void {
+    // TODO シェア内容
+    const text = encodeURIComponent(document.title);
+    const url = 'https://twitter.com/share?text=' + text + '&url=' + encodeURIComponent(location.href);
+    window.open(url, '_blank', 'noopener');
+  }
+
+  /**
+   * LINEでシェア
+   */
+  shareLine(): void {
+    // TODO シェア内容
+    const text = encodeURIComponent(document.title) + '%20' + encodeURIComponent(location.href);
+    const url = 'https://line.me/R/msg/text/?' + text;
+    window.open(url, '_blank', 'noopener');
+  }
+
+  /**
+   * リンクをコピー
+   */
+  copyLink(): void {
+    document.addEventListener('copy', (e: ClipboardEvent) => {
+      e.preventDefault();
+      e.clipboardData.setData('text/plain', location.href);
+    }, { once: true });
+    document.execCommand('copy');
+
+    alert('リンクがクリップボードにコピーされました。');
+  }
 }
