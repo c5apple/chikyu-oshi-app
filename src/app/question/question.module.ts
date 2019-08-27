756731b011318ea59c4f70edd3d1ac0483fb2d55
c5apple <c5apple_party@yahoo.co.jp>
2019-08-27 23:45:40 +0900

    #1 シェアボタン ガワのみ

--- src/app/question/question.module.ts
+++ src/app/question/question.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { QuestionComponent } from './question.component';
 import { AppRoutingModule } from '../app-routing.module';
+import { ShareModule } from '../shared/component/share/share.module';
 
 @NgModule({
   declarations: [QuestionComponent],
   imports: [
     CommonModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ShareModule
   ],
   exports: [
     QuestionComponent
---
+++ src/app/shared/component/share/share.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+/**
+ * シェアコンポーネント
+ */
+@Component({
+  selector: 'app-share',
+  templateUrl: './share.component.html',
+  styleUrls: ['./share.component.scss']
+})
+export class ShareComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
