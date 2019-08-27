a2e37153004ce212b3c83ac3cbf4f8704070e1cb
c5apple <c5apple_party@yahoo.co.jp>
2019-08-28 01:26:14 +0900

    #5 GoogleAnalyticsサービス

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
+import { filter } from 'rxjs/operators';
+import { GaService } from './shared/service/ga';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
 
+  constructor(
+    private router: Router,
+    private gaService: GaService
+  ) { }
+
+  ngOnInit() {
+    this.router.events.pipe(filter(event => event instanceof NavigationEnd)).subscribe((params: any) => {
+      window.scrollTo(0, 0);
+
+      // tracking
+      this.gaService.sendPageView(params.url);
+    });
+  }
 }
