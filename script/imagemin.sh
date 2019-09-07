6ee3df64a687aa9b62193d2b1b1c0f429aa30fc9
c5apple <c5apple_party@yahoo.co.jp>
2019-09-07 22:11:10 +0900

    画像圧縮コマンド

--- script/imagemin.sh
+++ script/imagemin.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-ls -l src/assets/img/
+ls -l "src/assets/img/$1"
 cp "src/assets/img/$1" ../imagemin-tool/src/.
 cd ../imagemin-tool/
 node index.js 200; git clean -f
 mv "dist/$1" ../chikyu-oshi-app/src/assets/img/.
 cd ../chikyu-oshi-app/
-ls -l src/assets/img/
+ls -l "src/assets/img/$1"
